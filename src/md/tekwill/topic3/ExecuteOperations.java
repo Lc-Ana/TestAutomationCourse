2d08f9ac436c2a38b8664c1600c21e67e2368fa7
Ana <lipcanana1997@gmail.com>
2022-03-20 20:33:27 +0200

    TAC-08: Added methods releted to Java Operations Topic

---
+++ src/md/tekwill/topic3/ExecuteOperations.java
@@ -0,0 +1,12 @@
+package md.tekwill.topic3;
+
+public class ExecuteOperations {
+    public static void main(String[] args) {
+//        Uncomment to run the code!
+//        Operations.printArithmeticOperations(5,6);
+//        Operations.printLogicOperations(true, false);
+//        Operations.printRelationalOperations(10,100);
+        Operations.userForAndWhile(1,5);
+    }
+
+}
